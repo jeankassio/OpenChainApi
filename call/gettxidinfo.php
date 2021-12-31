25f465e18e742705a476ff01cc681ba6f4a119c0
Jean Kassio <jeankassio@gmail.com>
2021-12-31 15:34:43 -0300

    Get Information TXID

--- call/gettxidinfo.php
+++ call/gettxidinfo.php
@@ -10,9 +10,9 @@ require_once(dirname(__FILE__, 2) . "/conn/easybitcoin.php");
 
 */
 
-$json_params = file_get_contents("php://input");
+$json_params = file_get_contents("php://input"); // Get the json data
 
-if(!isValidJSON($json_params)){
+if(!isValidJSON($json_params)){ //check validate of json
 	$response["code"] = 500;
 	$response["message"] = "Invalid call";
 	$response["date"] = date("Y-m-d H:i:s");		
@@ -20,9 +20,7 @@ if(!isValidJSON($json_params)){
 	exit();
 }
 
-$PARAMS = json_decode($json_params, true);
-
-
+$PARAMS = json_decode($json_params, true); //Convert json to array
 
 if(!isset($PARAMS['txid'])){
 	$response["code"] = 501;
@@ -31,11 +29,11 @@ if(!isset($PARAMS['txid'])){
 	echo json_encode($response, JSON_UNESCAPED_UNICODE);
 	exit();
 }
-$btc = new Bitcoin(USER_BITCOIN, PASS_BITCOIN);
+$btc = new Bitcoin(USER_BITCOIN, PASS_BITCOIN); //Connect to RPC
 
 $txid = $PARAMS['txid'];
 
-	if(($transaction = $btc->getrawtransaction($txid, 1)) === false){
+	if(($transaction = $btc->getrawtransaction($txid, 1)) === false){ //Get Raw Transaction and decode to json
 		
 		$response["code"] = 501;
 		$response["message"] = $btc->response['error']['message'];
@@ -49,11 +47,11 @@ $txid = $PARAMS['txid'];
 	$outputs = array();
 	$fee = 0;
 	
-	foreach($transaction['vin'] as $parent){
+	foreach($transaction['vin'] as $parent){ //Get the 'vin' of the master transaction
 		
-		$vout = $parent['vout'];
+		$vout = $parent['vout']; //get the 'vout' of the transaction parent
 		
-		if(($trparent = $btc->getrawtransaction($parent['txid'], 1)) === false){
+		if(($trparent = $btc->getrawtransaction($parent['txid'], 1)) === false){ //Get the transaction parent
 			
 			$response["code"] = 501;
 			$response["message"] = $btc->response['error']['message'];
@@ -63,25 +61,25 @@ $txid = $PARAMS['txid'];
 			
 		}
 		
-		$key = array_search($vout, array_column($trparent['vout'], 'n'));
+		$key = array_search($vout, array_column($trparent['vout'], 'n')); //Get the key of address referency in transaction parent 
 		
-		$inputs[] = array(
+		$inputs[] = array( //get vout of transaction parent 
 			'address' => $trparent['vout'][$key]['scriptPubKey']['address'],
 			'value' => number_format($trparent['vout'][$key]['value'], 8, ".", "")
 		);
 		
-		$fee += $trparent['vout'][$key]['value'];
+		$fee += $trparent['vout'][$key]['value']; //Add all value in
 		
 	}
 	
-	foreach($transaction['vout'] as $parent){
+	foreach($transaction['vout'] as $parent){ //get vout of transaction
 		
 		$outputs[] = array(
 			'address' => $parent['scriptPubKey']['address'],
 			'value' => number_format($parent['value'], 8, ".", "")
 		);
 		
-		$fee -= $parent['value'];
+		$fee -= $parent['value']; //Remove value out to get fee
 		
 	}
 	
