3d10f3bac17e22a03bb097fadb6ee40d7f76f3d0
Jean Kassio <jeankassio@gmail.com>
2021-12-31 15:59:42 -0300

    Correction calculate fee non segwit

--- call/calculatefee.php
+++ call/calculatefee.php
@@ -27,9 +27,9 @@ Necessary send POST JSON:
 
 */
 
-$json_params = file_get_contents("php://input");
+$json_params = file_get_contents("php://input"); // Get the json data
 
-if(!isValidJSON($json_params)){
+if(!isValidJSON($json_params)){ //check validate of json
 	$response["code"] = 500;
 	$response["message"] = "Invalid call";
 	$response["date"] = date("Y-m-d H:i:s");		
@@ -37,10 +37,9 @@ if(!isValidJSON($json_params)){
 	exit();
 }
 
+$PARAMS = json_decode($json_params, true); //Convert json to array
 
-$PARAMS = json_decode($json_params, true);
-
-if(!isset($PARAMS['inputs'],$PARAMS['outputs'],$PARAMS['wallet'],$PARAMS['pass'])){
+if(!isset($PARAMS['inputs'],$PARAMS['outputs'],$PARAMS['wallet'],$PARAMS['pass'])){ //check if call is right
 	$response["code"] = 501;
 	$response["message"] = "Invalid call";
 	$response["date"] = date("Y-m-d H:i:s");		
@@ -48,14 +47,15 @@ if(!isset($PARAMS['inputs'],$PARAMS['outputs'],$PARAMS['wallet'],$PARAMS['pass']
 	exit();
 }
 
-
-$btc = new Bitcoin(USER_BITCOIN, PASS_BITCOIN, SERVER_RPC, PORT_RPC, PATH_WALLET . $PARAMS['wallet']);
+$btc = new Bitcoin(USER_BITCOIN, PASS_BITCOIN, SERVER_RPC, PORT_RPC, PATH_WALLET . $PARAMS['wallet']); //Connect to RPC Wallet
 
 $inputs = $PARAMS['inputs'];
 $_outputs = $PARAMS['outputs'];
 $replaceable = true;
+$segwit = true;
 
-if(!is_null($btc->walletpassphrase($PARAMS['pass'], 30))){
+
+if(!is_null($btc->walletpassphrase($PARAMS['pass'], 30))){ //Decrypt Wallet
 	
 	$response["code"] = 301;
 	$response["message"] = "Error decoding wallet";
@@ -69,7 +69,7 @@ $addresses = array();
 
 foreach($inputs as $addrs){
 
-	if(($vaddr = $btc->validateaddress($addrs['address'])) === false OR ($vaddr['isvalid'] !== true)){
+	if(($vaddr = $btc->validateaddress($addrs['address'])) === false OR ($vaddr['isvalid'] !== true)){ //Check validate of address
 		$response["code"] = 1000;
 		$response["message"] = "Invalid address";
 		$response["date"] = date("Y-m-d H:i:s");		
@@ -77,7 +77,7 @@ foreach($inputs as $addrs){
 		exit();
 	}
 
-	if(($tpriv = $btc->dumpprivkey($addrs['address'])) === false){
+	if(($tpriv = $btc->dumpprivkey($addrs['address'])) === false){ //Get the private key
 		$response["code"] = 1001;
 		$response["message"] = "Invalid private key";
 		$response["date"] = date("Y-m-d H:i:s");		
@@ -85,7 +85,7 @@ foreach($inputs as $addrs){
 		exit();
 	}
 
-	if($tpriv !== $addrs['private']){
+	if($tpriv !== $addrs['private']){ //Compare private key to continue
 
 		$response["code"] = 1002;
 		$response["message"] = "Invalid private key";
@@ -95,12 +95,16 @@ foreach($inputs as $addrs){
 		
 	}
 	
+	if(substr($addrs['address'], 0, 1) != '3' AND substr($addrs['address'], 0, 3) != 'bc1'){
+		$segwit = false;
+	}
+	
 	$addresses[] = $addrs['address'];
 	
 }
 	
 	
-	if(($unspends = $btc->listunspent(0, 9999999, $addresses)) === false){
+	if(($unspends = $btc->listunspent(0, 9999999, $addresses)) === false){ //Get unspent values of inputs
 		$response["code"] = 1003;
 		$response["message"] = $btc->response['error']['message'];
 		$response["date"] = date("Y-m-d H:i:s");		
@@ -124,28 +128,32 @@ foreach($inputs as $addrs){
 			
 		}
 		
-		foreach($_outputs as $_output){
+		foreach($_outputs as $_output){ //Distribute the amount values in outputs
 			
 			$output[][trim($_output['recipient'])] =  number_format($_output['value'], 8, ".", "");
 			
 			$amount = number_format($amount - $_output['value'], 8, ".", "");
 			
+			if(substr($_output['recipient'], 0, 1) != '3' AND substr($_output['recipient'], 0, 3) != 'bc1'){
+				$segwit = false;
+			}			
+			
 		}
 	
 	
-	if($amount > 0){
+	if($amount > 0){ //If rest a change, send to a address in input
 		
 		$iaddr = $addresses[count($addresses) -1];
 		
-		if((array_key_exists($iaddr, $output)) === false){
+		if((array_key_exists($iaddr, $output)) === false){ //If not exist address input in output, add
 			
 			$output[][$iaddr] = $amount;
 			
-		}else{
+		}else{ 
 			
-			foreach($output as $ind=>$addr){
+			foreach($output as $ind=>$addr){ 
 				
-				if($addr[0] == $iaddr){
+				if($addr[0] == $iaddr){ //if exist, increase the value
 					$output[$ind][$iaddr] = number_format($output[$ind][$iaddr] + $amount, 8, ".", "");
 				}
 				
@@ -156,7 +164,7 @@ foreach($inputs as $addrs){
 	}
 	
 	
-	if(($rawtransaction = $btc->createrawtransaction($input, $output, 0, $replaceable)) === false){
+	if(($rawtransaction = $btc->createrawtransaction($input, $output, 0, $replaceable)) === false){ //create the raw transaction
 			$response["code"] = 1004;
 			$response["message"] = $btc->response['error']['message'];
 			$response["date"] = date("Y-m-d H:i:s");		
@@ -164,7 +172,7 @@ foreach($inputs as $addrs){
 			exit();
 	}
 	
-	if(($signrawtransaction = $btc->signrawtransactionwithwallet($rawtransaction)) === false){
+	if(($signrawtransaction = $btc->signrawtransactionwithwallet($rawtransaction)) === false){ //sign the raw transaction
 			$response["code"] = 1005;
 			$response["message"] = $btc->response['error']['message'];
 			$response["date"] = date("Y-m-d H:i:s");		
@@ -172,7 +180,7 @@ foreach($inputs as $addrs){
 			exit();
 	}
 	
-	if(($decoderawtransaction = $btc->decoderawtransaction($signrawtransaction['hex'])) === false){
+	if(($decoderawtransaction = $btc->decoderawtransaction($signrawtransaction['hex'])) === false){ //decode raw transaction with hex sign
 			$response["code"] = 1006;
 			$response["message"] = $btc->response['error']['message'];
 			$response["date"] = date("Y-m-d H:i:s");		
@@ -180,7 +188,7 @@ foreach($inputs as $addrs){
 			exit();
 	}
 	
-	if(($feeperkbyte = $btc->estimatesmartfee(6, "CONSERVATIVE")) === false){
+	if(($feeperkbyte = $btc->estimatesmartfee(6, "CONSERVATIVE")) === false){ //get fee per kylobyte in fast fee
 			$response["code"] = 1007;
 			$response["message"] = $btc->response['error']['message'];
 			$response["date"] = date("Y-m-d H:i:s");		
@@ -188,11 +196,11 @@ foreach($inputs as $addrs){
 			exit();
 	}
 	
-	$size = (true ? $decoderawtransaction['vsize'] : $decoderawtransaction['size']);
+	$size = ($segwit ? $decoderawtransaction['vsize'] : $decoderawtransaction['size']);
 	
 	$conservative = number_format(($feeperkbyte['feerate'] / 1000) * $size, 8, ".", "");
 	
-	if(($feeperkbyte = $btc->estimatesmartfee(6, "ECONOMICAL")) === false){
+	if(($feeperkbyte = $btc->estimatesmartfee(6, "ECONOMICAL")) === false){ //get fee per kylobyte in economical fee
 			$response["code"] = 1007;
 			$response["message"] = $btc->response['error']['message'];
 			$response["date"] = date("Y-m-d H:i:s");		
