4260ad32c1903169c7243cca4bf7ea8260c5ce18
Jean Kassio <jeankassio@gmail.com>
2021-12-31 15:21:39 -0300

    Generate a new Address

--- call/getnewaddress.php
+++ call/getnewaddress.php
@@ -13,9 +13,9 @@ require_once(dirname(__FILE__, 2) . "/conn/easybitcoin.php");
 
 */
 
-$json_params = file_get_contents("php://input");
+$json_params = file_get_contents("php://input"); // Get the json data
 
-if(!isValidJSON($json_params)){
+if(!isValidJSON($json_params)){ //check validate of json
 	$response["code"] = 500;
 	$response["message"] = "Invalid call";
 	$response["date"] = date("Y-m-d H:i:s");		
@@ -23,27 +23,27 @@ if(!isValidJSON($json_params)){
 	exit();
 }
 
-$PARAMS = json_decode($json_params, true);
+$PARAMS = json_decode($json_params, true); //Convert json to array
 
-if(!isset($PARAMS['conf'],$PARAMS['wallet'],$PARAMS['pass'],$PARAMS['hook'])){
+if(!isset($PARAMS['conf'],$PARAMS['wallet'],$PARAMS['pass'],$PARAMS['hook'])){ //Check if call is
 	$response["code"] = 501;
 	$response["message"] = "Invalid call";
 	$response["date"] = date("Y-m-d H:i:s");		
 	echo json_encode($response, JSON_UNESCAPED_UNICODE);
 	exit();
-}elseif(is_int($PARAMS['conf'])){
+}elseif(is_int($PARAMS['conf'])){ //Check if confirmation is a number valid
 	$response["code"] = 302;
 	$response["message"] = "Invalid format of number confirmations";
 	$response["date"] = date("Y-m-d H:i:s");		
 	echo json_encode($response, JSON_UNESCAPED_UNICODE);
 	exit();
-}elseif(!is_null($PARAMS['hook']) AND !filter_var($PARAMS['hook'], FILTER_VALIDATE_URL)){
+}elseif(!is_null($PARAMS['hook']) AND !filter_var($PARAMS['hook'], FILTER_VALIDATE_URL)){ //Check if the hook parameter is a url or not defined
 	$response["code"] = 303;
 	$response["message"] = "Invalid webhook URL";
 	$response["date"] = date("Y-m-d H:i:s");		
 	echo json_encode($response, JSON_UNESCAPED_UNICODE);
 	exit();
-}elseif(strlen($PARAMS['wallet']) < 4 OR strlen($PARAMS['wallet']) > 30 OR strlen($PARAMS['pass']) < 6 OR strlen($PARAMS['pass']) > 80){
+}elseif(strlen($PARAMS['wallet']) < 4 OR strlen($PARAMS['wallet']) > 30 OR strlen($PARAMS['pass']) < 6 OR strlen($PARAMS['pass']) > 80){ //Check rules of lenght of name and pass of wallet
 	$response["code"] = 304;
 	$response["message"] = "Incorret lenght in wallet name or password";
 	$response["date"] = date("Y-m-d H:i:s");		
@@ -51,9 +51,9 @@ if(!isset($PARAMS['conf'],$PARAMS['wallet'],$PARAMS['pass'],$PARAMS['hook'])){
 	exit();
 }
 
-$btc = new Bitcoin(USER_BITCOIN, PASS_BITCOIN, SERVER_RPC, PORT_RPC, PATH_WALLET . $PARAMS['wallet']);
+$btc = new Bitcoin(USER_BITCOIN, PASS_BITCOIN, SERVER_RPC, PORT_RPC, PATH_WALLET . $PARAMS['wallet']); //create a new connection to RPC wallet
 
-if(!is_null($btc->walletpassphrase($PARAMS['pass'], 30))){
+if(!is_null($btc->walletpassphrase($PARAMS['pass'], 30))){ //decrypt the wallet
 	
 	$response["code"] = 301;
 	$response["message"] = "Error decoding wallet";
@@ -63,27 +63,16 @@ if(!is_null($btc->walletpassphrase($PARAMS['pass'], 30))){
 	
 }
 
-$public = $btc->getnewaddress();
-$private = $btc->dumpprivkey($public);
+$public = $btc->getnewaddress(); //generate a new address
+$private = $btc->dumpprivkey($public); //get a private key of this address
 
-SaveAddress($public, $PARAMS['conf'], $PARAMS['hook']);
-
-$btc->walletlock();
+SaveAddress($public, $PARAMS['conf'], $PARAMS['hook']); //save address, confirmations e hook if exists
 
 $response["code"] = 200;
-$response["info"] = array(
+$response["info"] = array( //return public and private key
 	'public' => $public,
 	'private' => $private
 );
 $response["date"] = date("Y-m-d H:i:s");		
 echo json_encode($response, JSON_UNESCAPED_UNICODE);
 exit();
-
-
-/*
-
-pegar endereço gerado
-buscar chave privada dele
-retornar endereço + chave privada
-
-*/
